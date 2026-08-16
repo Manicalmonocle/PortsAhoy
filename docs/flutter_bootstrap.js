1aca8594040adc82481951202468c8c071977178
Kevin Page <kpage@stevesilver.com>
2026-08-16 10:25:23 -0500

    Playtest build 1.0.6+7: run report export

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2893840766" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2065536052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
