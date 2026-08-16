966f33c6a5400f716e85ca0c3a5f2198132dc204
Kevin Page <kpage@stevesilver.com>
2026-08-16 01:04:54 -0500

    Playtest build 1.0.2+3: persistence, charter clarity, berth and commission fixes

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1369044785" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "124137083" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
